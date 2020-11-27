e2c78d6bc06a8b5e7f63a32543d46c40726bbeab
741186547 <741186547@qq.com>
2020-11-27 17:20:20 +0800

    song

---
+++ Web.WMS/Controllers/SeePallet/SeePalletController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.WMS.Controllers.SeePallet
+{
+    public class SeePalletController : Controller
+    {
+        /// <summary>
+        /// 返回主视图
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
