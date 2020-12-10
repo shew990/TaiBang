f276d66355e0cfb61546ef47d20cb2b2153a1c92
741186547 <741186547@qq.com>
2020-12-10 15:33:17 +0800

    song

--- SqlSugarDAL/view_checkrecord/View_CheckRecordService.cs
+++ SqlSugarDAL/view_checkrecord/View_CheckRecordService.cs
@@ -8,15 +8,14 @@ namespace SqlSugarDAL.view_checkrecord
 {
     public class View_CheckRecordService : DbContext<View_CheckRecord>
     {
-        public object GetOrderList(int limit, int page)
+        public object GetOrderList(int limit, int page, string OrderNo, string StartDate, string EndDate)
         {
-            var records = GetSugarQueryable().ToList();
-            var data = records.Skip(limit * (page - 1)).Take(limit);
+            var records = GetRecords(OrderNo, StartDate, EndDate);
             return new
             {
                 Result = 1,
-                ResultValue = (records == null || records.Count() == 0) ? "没有数据" : "",
-                Data = data,
+                ResultValue = (records == null || records.Count() == 0) ? "没有符合条件的数据" : "",
+                Data = records.Skip(limit * (page - 1)).Take(limit).ToList(),
                 PageData = new
                 {
                     totalCount = records.Count(),
@@ -27,6 +26,25 @@ namespace SqlSugarDAL.view_checkrecord
                     : (records.Count() / limit)
                 }
             };
+        }
+
+        /// <summary>
+        /// 根据条件筛选 质检记录 数据
+        /// </summary>
+        /// <param name="OrderNo"></param>
+        /// <param name="StartDate"></param>
+        /// <param name="EndDate"></param>
+        /// <returns></returns>
+        public List<View_CheckRecord> GetRecords(string OrderNo, string StartDate, string EndDate)
+        {
+            var records = GetSugarQueryable();
+            if (!string.IsNullOrEmpty(OrderNo))
+                records = records.Where(x => x.ErpVoucherNo.Contains(OrderNo));
+            if (!string.IsNullOrEmpty(StartDate))
+                records = records.Where(x => x.SaveTime >= Convert.ToDateTime(StartDate));
+            if (!string.IsNullOrEmpty(EndDate))
+                records = records.Where(x => x.SaveTime <= Convert.ToDateTime(EndDate));
+            return records.ToList();
         }
 
     }
