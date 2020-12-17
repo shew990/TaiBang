0669d4553e925a78f144326b3abe9e6b6ce1b92e
741186547 <741186547@qq.com>
2020-12-17 16:37:59 +0800

    song

--- Web.WMS/Controllers/QualityDetail/QualityDetailController.cs
+++ Web.WMS/Controllers/QualityDetail/QualityDetailController.cs
@@ -35,174 +35,21 @@ namespace Web.WMS.Controllers
         /// 跳转主视图
         /// </summary>
         /// <returns></returns>
-        public ActionResult GetModelList()
-        {
-            ViewBag.StrongHoldCode = strongHoldCode;
-            return View();
-        }
-
-        /// <summary>
-        /// 获取备注数据
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult GetRemarks()
-        {
-            var remarks = new RemarkService().GetList().Select(x => x.RemarkDesc);
-            return Json(remarks, JsonRequestBehavior.AllowGet);
-        }
-
-        /// <summary>
-        /// 根据生产单号获取订单数据
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult GetModel(string orderNo)
-        {
-            var model = new ProductService().GetList(x => x.ErpVoucherNo == orderNo).FirstOrDefault();
-            T_CheckRecord checkRecord = new T_CheckRecord();
-            if (model != null)
-                checkRecord = new CheckRecordService().GetList(x => x.ProductOrderId == model.id).FirstOrDefault();
-            return Json(new { product = model, record = checkRecord }, JsonRequestBehavior.AllowGet);
-        }
-
-        /// <summary>
-        /// 提交
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Submit(string formJson, string orderId, string qualityQty, string unQualityQty, string remark)
-        {
-            SuccessResult successResult = new SuccessResult();
-            successResult.Success = false;
-            try
-            {
-                CheckRecordService checkRecordService = new CheckRecordService();
-                ProductService productService = new ProductService();
-                var checkRecord = JsonConvert.DeserializeObject<T_CheckRecord>(formJson);
-                var queryData = checkRecordService
-                    .GetList(x => x.ProductOrderId == Convert.ToInt32(orderId)).FirstOrDefault();
-                if (queryData == null)
-                {
-                    queryData = new T_CheckRecord();
-                    queryData.TotalUnqualifiedNumber =
-                        unQualityQty == "" ? 0 : Convert.ToInt32(unQualityQty);//总不合格数
-
-                    //一部/二部
-                    queryData.Burning = checkRecord.Burning;
-                    queryData.WrongLabe = checkRecord.WrongLabe;
-                    queryData.Resistance = checkRecord.Resistance;
-                    queryData.WrongNumberControl = checkRecord.WrongNumberControl;
-                    queryData.DielectricStrength = checkRecord.DielectricStrength;
-                    queryData.GearBump = checkRecord.GearBump;
-                    queryData.AbnormalNoise = checkRecord.AbnormalNoise;
-                    queryData.BearingFailure = checkRecord.BearingFailure;
-                    queryData.InputPort = checkRecord.InputPort;
-                    queryData.OutPort = checkRecord.OutPort;
-                    queryData.MountingFlange = checkRecord.MountingFlange;
-                    queryData.InstallKeyway = checkRecord.InstallKeyway;
-                    queryData.InstallationShaftDiameter = checkRecord.InstallationShaftDiameter;
-                    queryData.InstallTheStop = checkRecord.InstallTheStop;
-                    queryData.BoltCenter = checkRecord.BoltCenter;
-                    queryData.NoLoad = checkRecord.NoLoad;
-                    queryData.RawMaterial = checkRecord.RawMaterial;
-                    queryData.Contour = checkRecord.Contour;
-                    queryData.SaveTime = DateTime.Now;
-                    queryData.ProductOrderId = Convert.ToInt32(orderId);
-
-                    //三部
-                    queryData.Sensei = checkRecord.Sensei;
-                    queryData.Scratch = checkRecord.Scratch;
-                    queryData.Bruise = checkRecord.Bruise;
-                    queryData.Speckle = checkRecord.Speckle;
-                    queryData.DownEdge = checkRecord.DownEdge;
-                    queryData.Rust = checkRecord.Rust;
-                    queryData.MissedProcess = checkRecord.MissedProcess;
-                    queryData.Decibel = checkRecord.Decibel;
-                    queryData.Dot = checkRecord.Dot;
-                    queryData.Disaccord = checkRecord.Disaccord;
-                    queryData.Noise = checkRecord.Noise;
-                    queryData.CardPoint = checkRecord.CardPoint;
-                    queryData.ShaftTight = checkRecord.ShaftTight;
-                    queryData.Shake = checkRecord.Shake;
-                    queryData.OutputSize = checkRecord.OutputSize;
-                    queryData.InPutSize = checkRecord.InPutSize;
-                    queryData.NeglectedLoading = checkRecord.NeglectedLoading;
-                    queryData.NotInPlace = checkRecord.NotInPlace;
-                    queryData.Others = checkRecord.Others;
-                    queryData.Minute = checkRecord.Minute;
-                    checkRecordService.Insert(queryData);
-                }
-                else
-                {
-                    queryData.TotalUnqualifiedNumber +=
-                        unQualityQty == "" ? 0 : Convert.ToInt32(unQualityQty);//总不合格数
-
-                    queryData.Sensei = checkRecord.Sensei;
-                    queryData.Scratch = checkRecord.Scratch;
-                    queryData.Bruise = checkRecord.Bruise;
-                    queryData.Speckle = checkRecord.Speckle;
-                    queryData.DownEdge = checkRecord.DownEdge;
-                    queryData.Rust = checkRecord.Rust;
-                    queryData.MissedProcess = checkRecord.MissedProcess;
-                    queryData.Decibel = checkRecord.Decibel;
-                    queryData.Dot = checkRecord.Dot;
-                    queryData.Disaccord = checkRecord.Disaccord;
-                    queryData.Noise = checkRecord.Noise;
-                    queryData.CardPoint = checkRecord.CardPoint;
-                    queryData.ShaftTight = checkRecord.ShaftTight;
-                    queryData.Shake = checkRecord.Shake;
-                    queryData.OutputSize = checkRecord.OutputSize;
-                    queryData.InPutSize = checkRecord.InPutSize;
-                    queryData.NeglectedLoading = checkRecord.NeglectedLoading;
-                    queryData.NotInPlace = checkRecord.NotInPlace;
-                    queryData.Others = checkRecord.Others;
-                    queryData.Minute = checkRecord.Minute;
-
-                    queryData.Burning = checkRecord.Burning;
-                    queryData.WrongLabe = checkRecord.WrongLabe;
-                    queryData.Resistance = checkRecord.Resistance;
-                    queryData.WrongNumberControl = checkRecord.WrongNumberControl;
-                    queryData.DielectricStrength = checkRecord.DielectricStrength;
-                    queryData.GearBump = checkRecord.GearBump;
-                    queryData.AbnormalNoise = checkRecord.AbnormalNoise;
-                    queryData.BearingFailure = checkRecord.BearingFailure;
-                    queryData.InputPort = checkRecord.InputPort;
-                    queryData.OutPort = checkRecord.OutPort;
-                    queryData.MountingFlange = checkRecord.MountingFlange;
-                    queryData.InstallKeyway = checkRecord.InstallKeyway;
-                    queryData.InstallationShaftDiameter = checkRecord.InstallationShaftDiameter;
-                    queryData.InstallTheStop = checkRecord.InstallTheStop;
-                    queryData.BoltCenter = checkRecord.BoltCenter;
-                    queryData.ProductOrderId = Convert.ToInt32(orderId);
-                    queryData.NoLoad = checkRecord.NoLoad;
-                    queryData.RawMaterial = checkRecord.RawMaterial;
-                    queryData.Contour = checkRecord.Contour;
-                    queryData.SaveTime = DateTime.Now;
-                    checkRecordService.Update(queryData);
-                }
-                var product = productService.GetById(orderId);
-                product.QulityQty += Convert.ToDecimal(qualityQty);
-                product.Remark = remark;
-                productService.Update(product);
-
-                successResult.Msg = "保存成功!";
-                successResult.Success = true;
-            }
-            catch (Exception ex)
-            {
-                successResult.Msg = ex.Message;
-            }
-            return Json(successResult, JsonRequestBehavior.AllowGet);
-        }
-
-        /// <summary>
-        /// 跳转导出页面
-        /// </summary>
-        /// <returns></returns>
         public ActionResult CheckRecordOut()
         {
             ViewBag.strongHoldCode = strongHoldCode;
             return View();
         }
 
+        /// <summary>
+        /// 获取表格数据
+        /// </summary>
+        /// <param name="limit"></param>
+        /// <param name="page"></param>
+        /// <param name="OrderNo"></param>
+        /// <param name="StartDate"></param>
+        /// <param name="EndDate"></param>
+        /// <returns></returns>
         public ActionResult GetOrderList(int limit, int page, string OrderNo, string StartDate, string EndDate)
         {
             var ordersObj = new View_CheckRecordService().GetOrderList(limit, page, OrderNo, StartDate, EndDate);
@@ -357,5 +204,114 @@ namespace Web.WMS.Controllers
 
             return File(memoryStream, "application/vnd.ms-excel", fileName);
         }
+
+        /// <summary>
+        /// 跳转质检页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetModelList(string checkRecordId = null)
+        {
+            ViewBag.record = checkRecordId == null ? null : new View_CheckRecordService().GetById(checkRecordId);
+            ViewBag.StrongHoldCode = strongHoldCode;
+            return View();
+        }
+
+        /// <summary>
+        /// 获取备注数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetRemarks()
+        {
+            var remarks = new RemarkService().GetList().Select(x => x.RemarkDesc);
+            return Json(remarks, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 根据生产单号获取订单数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetModel(string orderNo)
+        {
+            var model = new ProductService().GetList(x => x.ErpVoucherNo == orderNo).FirstOrDefault();
+            T_CheckRecord checkRecord = new T_CheckRecord();
+            if (model != null)
+                checkRecord = new CheckRecordService().GetList(x => x.ProductOrderId == model.id).FirstOrDefault();
+            return Json(new { product = model, record = checkRecord }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// 提交
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Submit(string formJson, string orderId, string qualityQty, string remark)
+        {
+            SuccessResult successResult = new SuccessResult();
+            successResult.Success = false;
+            try
+            {
+                ProductService productService = new ProductService();
+                var checkRecord = JsonConvert.DeserializeObject<T_CheckRecord>(formJson);
+
+                T_CheckRecord queryData = new T_CheckRecord();
+                //一部/二部
+                queryData.Burning = checkRecord.Burning;
+                queryData.WrongLabe = checkRecord.WrongLabe;
+                queryData.Resistance = checkRecord.Resistance;
+                queryData.WrongNumberControl = checkRecord.WrongNumberControl;
+                queryData.DielectricStrength = checkRecord.DielectricStrength;
+                queryData.GearBump = checkRecord.GearBump;
+                queryData.AbnormalNoise = checkRecord.AbnormalNoise;
+                queryData.BearingFailure = checkRecord.BearingFailure;
+                queryData.InputPort = checkRecord.InputPort;
+                queryData.OutPort = checkRecord.OutPort;
+                queryData.MountingFlange = checkRecord.MountingFlange;
+                queryData.InstallKeyway = checkRecord.InstallKeyway;
+                queryData.InstallationShaftDiameter = checkRecord.InstallationShaftDiameter;
+                queryData.InstallTheStop = checkRecord.InstallTheStop;
+                queryData.BoltCenter = checkRecord.BoltCenter;
+                queryData.NoLoad = checkRecord.NoLoad;
+                queryData.RawMaterial = checkRecord.RawMaterial;
+                queryData.Contour = checkRecord.Contour;
+                queryData.SaveTime = DateTime.Now;
+                queryData.ProductOrderId = Convert.ToInt32(orderId);
+
+                //三部
+                queryData.Sensei = checkRecord.Sensei;
+                queryData.Scratch = checkRecord.Scratch;
+                queryData.Bruise = checkRecord.Bruise;
+                queryData.Speckle = checkRecord.Speckle;
+                queryData.DownEdge = checkRecord.DownEdge;
+                queryData.Rust = checkRecord.Rust;
+                queryData.MissedProcess = checkRecord.MissedProcess;
+                queryData.Decibel = checkRecord.Decibel;
+                queryData.Dot = checkRecord.Dot;
+                queryData.Disaccord = checkRecord.Disaccord;
+                queryData.Noise = checkRecord.Noise;
+                queryData.CardPoint = checkRecord.CardPoint;
+                queryData.ShaftTight = checkRecord.ShaftTight;
+                queryData.Shake = checkRecord.Shake;
+                queryData.OutputSize = checkRecord.OutputSize;
+                queryData.InPutSize = checkRecord.InPutSize;
+                queryData.NeglectedLoading = checkRecord.NeglectedLoading;
+                queryData.NotInPlace = checkRecord.NotInPlace;
+                queryData.Others = checkRecord.Others;
+                queryData.Minute = checkRecord.Minute;
+                new CheckRecordService().Insert(queryData);
+
+                var product = productService.GetById(orderId);
+                product.QulityQty += Convert.ToDecimal(qualityQty);
+                product.Remark = remark;
+                productService.Update(product);
+
+                successResult.Msg = "保存成功!";
+                successResult.Success = true;
+            }
+            catch (Exception ex)
+            {
+                successResult.Msg = ex.Message;
+            }
+            return Json(successResult, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
