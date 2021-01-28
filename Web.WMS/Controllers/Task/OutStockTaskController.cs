1036f1da8e38804c771bae1a65d6c5332799a2a6
741186547 <741186547@qq.com>
2021-01-28 16:19:24 +0800

    song

--- Web.WMS/Controllers/Task/OutStockTaskController.cs
+++ Web.WMS/Controllers/Task/OutStockTaskController.cs
@@ -1,4 +1,5 @@
-﻿using BILWeb.Login.User;
+﻿using BILBasic.Common;
+using BILWeb.Login.User;
 using BILWeb.OutStock;
 using BILWeb.OutStockTask;
 using System;
@@ -36,52 +37,36 @@ namespace Web.WMS.Controllers.Task
 
         public FileResult Excel(T_OutStockTaskInfo model)
         {
+            DividPage page = new DividPage() { CurrentPageShowCounts=10000000};
             string strErrMsg = string.Empty;
-            List<T_OutStockTaskDetailsInfo> lstExport = new List<T_OutStockTaskDetailsInfo>();
-            tfunc_detail.GetExportTaskDetail(model, ref lstExport, ref strErrMsg);
+            List <T_OutStockTaskInfo> lstExport = new List<T_OutStockTaskInfo>();
+            tfunc_task.GetModelListByPage(ref lstExport, currentUser, model, ref page, ref strErrMsg);
             //创建Excel文件的对象
             NPOI.HSSF.UserModel.HSSFWorkbook book = new NPOI.HSSF.UserModel.HSSFWorkbook();
             //添加一个sheet
             NPOI.SS.UserModel.ISheet sheet1 = book.CreateSheet("Sheet1");
             //给sheet1添加第一行的头部标题
             NPOI.SS.UserModel.IRow row1 = sheet1.CreateRow(0);
-            row1.CreateCell(0).SetCellValue("任务号");
+            row1.CreateCell(0).SetCellValue("据点名");
             row1.CreateCell(1).SetCellValue("单据类型");
             row1.CreateCell(2).SetCellValue("ERP单号");
-            row1.CreateCell(4).SetCellValue("物料名称");
-            row1.CreateCell(3).SetCellValue("物料号");
-            row1.CreateCell(4).SetCellValue("物料名称");
-            row1.CreateCell(5).SetCellValue("任务数");
-            row1.CreateCell(6).SetCellValue("剩余数");
-            row1.CreateCell(9).SetCellValue("下架数");
-            row1.CreateCell(7).SetCellValue("操作人");
-            row1.CreateCell(8).SetCellValue("操作时间");
-            row1.CreateCell(9).SetCellValue("创建人");
-            row1.CreateCell(10).SetCellValue("创建时间");
-            row1.CreateCell(11).SetCellValue("供应商编号");
-            row1.CreateCell(12).SetCellValue("供应商");
-            row1.CreateCell(13).SetCellValue("状态");
-            //row1.CreateCell(14).SetCellValue("拣货车");
-            //row1.CreateCell(15).SetCellValue("地标");
+            row1.CreateCell(3).SetCellValue("任务号");
+            row1.CreateCell(4).SetCellValue("供应商");
+            row1.CreateCell(5).SetCellValue("状态");
+            row1.CreateCell(6).SetCellValue("创建时间");
+            row1.CreateCell(7).SetCellValue("仓库");
             //将数据逐步写入sheet1各个行
             for (int i = 0; i < lstExport.Count; i++)
             {
                 NPOI.SS.UserModel.IRow rowtemp = sheet1.CreateRow(i + 1);
-                rowtemp.CreateCell(0).SetCellValue(lstExport[i].TaskNo);
+                rowtemp.CreateCell(0).SetCellValue(lstExport[i].StrongHoldName);
                 rowtemp.CreateCell(1).SetCellValue(lstExport[i].StrVoucherType);
                 rowtemp.CreateCell(2).SetCellValue(lstExport[i].ErpVoucherNo);
-                rowtemp.CreateCell(3).SetCellValue(lstExport[i].MaterialNo);
-                rowtemp.CreateCell(4).SetCellValue(lstExport[i].MaterialDesc);
-                rowtemp.CreateCell(5).SetCellValue(lstExport[i].TaskQty.ToString());
-                rowtemp.CreateCell(6).SetCellValue(lstExport[i].RemainQty.ToString());
-                rowtemp.CreateCell(7).SetCellValue(lstExport[i].UnShelveQty.ToString());
-                rowtemp.CreateCell(8).SetCellValue(lstExport[i].OperatorUserName.ToString());
-                rowtemp.CreateCell(9).SetCellValue(lstExport[i].OperatorDateTime.ToString());
-                rowtemp.CreateCell(10).SetCellValue(lstExport[i].Creater.ToString());
-                rowtemp.CreateCell(11).SetCellValue(lstExport[i].CreateTime.ToString());
-                rowtemp.CreateCell(12).SetCellValue(lstExport[i].SupCusCode);
-                rowtemp.CreateCell(13).SetCellValue(lstExport[i].StrStatus);
-                //rowtemp.CreateCell(14).SetCellValue(lstExport[i].CarNo);
+                rowtemp.CreateCell(3).SetCellValue(lstExport[i].TaskNo);
+                rowtemp.CreateCell(4).SetCellValue(lstExport[i].SupcusName);
+                rowtemp.CreateCell(5).SetCellValue(lstExport[i].StrStatus);
+                rowtemp.CreateCell(6).SetCellValue(lstExport[i].CreateTime.ToString());
+                rowtemp.CreateCell(7).SetCellValue(lstExport[i].WareHouseName);
             }
             // 写入到客户端 
             System.IO.MemoryStream ms = new System.IO.MemoryStream();
