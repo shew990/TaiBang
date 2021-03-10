2cb376752fb96a19648fb5fe2404c75042d01248
741186547 <741186547@qq.com>
2021-03-10 17:20:05 +0800

    song

---
+++ Web.WMS/Controllers/SeePalletTable/SeePalletTableController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.WMS.Controllers.SeePalletTable
+{
+    public class SeePalletTableController : Controller
+    {
+        // GET: SeePalletTable
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
