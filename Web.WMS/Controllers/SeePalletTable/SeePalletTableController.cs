5838014c855cd0ce73d4fdcffd2d8de9da30caf1
741186547 <741186547@qq.com>
2021-03-11 15:29:34 +0800

    song

--- Web.WMS/Controllers/SeePalletTable/SeePalletTableController.cs
+++ Web.WMS/Controllers/SeePalletTable/SeePalletTableController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SqlSugarDAL.wareHouse;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,9 +14,24 @@ namespace Web.WMS.Controllers.SeePalletTable
         /// 跳转主视图
         /// </summary>
         /// <returns></returns>
-        public ActionResult Index()
+        public ActionResult Index(string houseNo = null, string orderType = null)
         {
-            return View();
+            var houses = new WareHouseService().GetList();
+            var refreshTime = Convert.ToInt32(ConfigurationManager.AppSettings["RefreshTime"]) * 1000;
+
+            ViewBag.refreshTime = refreshTime;
+            ViewBag.houseNo = houseNo == null ? "" : houseNo;
+            ViewBag.orderType = orderType == null ? "" : orderType;
+            return View(houses);
+        }
+
+        /// <summary>
+        /// 表格数据
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List(int limit, int page,string WAREHOUSENO,string OrderType)
+        {
+            return Json("", JsonRequestBehavior.AllowGet);
         }
     }
 }
