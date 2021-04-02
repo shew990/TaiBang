fa879a8073e294d561eb8808e7e5142caa243302
741186547 <741186547@qq.com>
2021-04-02 16:39:53 +0800

    song

--- SqlSugarDAL/KanBan.cs
+++ SqlSugarDAL/KanBan.cs
@@ -116,10 +116,5 @@ namespace SqlSugarDAL
         /// </summary>
         public String BackColor { get; set; }
 
-        /// <summary>
-        /// 预备货人
-        /// </summary>
-        public String PrepareMan { get; set; }
-
     }
 }
